f3126c246dd09dfad2f7191f839a0d751d672d15
Nyasha Mafirakureva <mafirakurevan@gmail.com>
2019-08-28 20:51:24 +0100

    Using crude birth rates to estimate TB in pregnancy
    The results are now much closer to the Sugarman et al 2014 estimates

---
+++ pregTB_CBR.R
@@ -0,0 +1,347 @@
+# Pregnancy and TB data
+
+# Aim: construct a single merged dataset at country level
+
+# Sources of data
+# http://www.who.int/tb/country/data/download/en/
+# https://extranet.who.int/tme/generateCSV.asp?ds=dictionary
+# https://population.un.org/wpp/Download/Standard/Population/
+# https://population.un.org/wpp/Download/Standard/Fertility/
+
+cat("\014") # clears Console (in RStudio)
+sessionInfo() # gives session info, ver of R, packages
+rm(list=ls()) #removes work space environment
+
+library(readxl)
+library(data.table)
+library(tidyverse)
+library(viridis)
+library(getTBinR)
+library(dplyr)
+library(plyr)
+
+# setwd("U:/Documents/GitHub/pregTB")
+setwd("~/Documents/GitHub/pregtb")
+
+# read in WHO TB data disaggregated by age and sex
+# read in the data dictionary 
+# Estimated number of incident cases (all forms) - Worked it out from the main dataset???
+df<- read.csv("https://extranet.who.int/tme/generateCSV.asp?ds=estimates_age_sex", header = T)
+df_dic<-read.csv("https://extranet.who.int/tme/generateCSV.asp?ds=dictionary")
+
+# Filter females of reproductive age group
+# age groups selected to match the UN population age groups for now
+df_1 <- df %>% filter(sex=="f", age_group %in% c("15-24", "25-34", "35-44", "45-54"))
+
+# Crude birth rate and births 
+# Crude birth rate (births per 1,000 population)								
+cbr_average<- read_csv("~/Downloads/WPP2019_Period_Indicators_Medium.csv")  %>% select(Location, Variant, Time, CBR, Births)
+cbr_lohi<- read_csv("WPP2019_Period_Indicators_OtherVariants.csv") %>% select(Location, Variant, Time, CBR, Births)
+
+cbr <- rbind(cbr_average, cbr_lohi)
+
+for (k in 1:length(names(cbr))){ #convert all the rows with type 'integer' to 'numeric
+  if (is.integer(cbr[,k])){
+    cbr[,k] <- as.numeric(cbr[,k])	
+  }
+}
+
+
+# names(cbr)[1] <- "country"
+cbr <- cbr %>% dplyr::rename(country=Location, year=Time)
+# births <- births %>% select(-c("15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49")) %>% 
+#   gather(age_group, estimate, 7:10)
+
+cbr <- separate(cbr, year, c("from", "to"))
+cbr$to <- as.numeric(cbr$to)-1
+cbr$year <- mapply(seq,cbr$from,cbr$to,SIMPLIFY=FALSE)
+
+cbr <- cbr %>% 
+  unnest(year) %>% 
+  select(-from,-to) %>% 
+  filter(Variant %in% c("Medium", "High", "Low")) 
+
+
+cbr_wide <- cbr %>% 
+  group_by_at(vars(-CBR, -year, -Births)) %>%  # group by everything other than the value column. 
+  mutate(row_id=1:n()) %>% ungroup() %>%  # build group index
+  nest(CBR, Births, .key = 'value_col') %>%
+  spread(key=Variant, value=value_col) %>% 
+  unnest(High, Low, Medium, .sep = '_') %>% # spread
+  select(-row_id)  # drop the index
+
+
+# cbr_wide <- cbr_wide %>% dplyr::rename(CBR_lo=Low, CBR_hi=High, CBR_best=Medium)
+# some country names need sorting out
+cbr_wide <- cbr_wide %>% filter(!country %in%
+                              c("WORLD", "More developed regions",
+                                "Less developed regions",
+                                "Least developed countries",
+                                "Less developed regions, excluding least developed countries",
+                                "Less developed regions, excluding China",
+                                "High-income countries",
+                                "Middle-income countries",
+                                "Upper-middle-income countries",
+                                "Lower-middle-income countries",
+                                "Low-income countries",
+                                "Sub-Saharan Africa",
+                                "AFRICA",
+                                "Africa",
+                                "Eastern Africa",
+                                "Middle Africa",
+                                "Northern Africa",
+                                "Western Sahara",
+                                "Southern Africa",
+                                "Western Africa",
+                                "ASIA",
+                                "Asia",
+                                "Eastern Asia",
+                                "South-Central Asia",
+                                "Central Asia",
+                                "Southern Asia",
+                                "South-Eastern Asia",
+                                "Western Asia",
+                                "EUROPE",
+                                "Europe",
+                                "Eastern Europe",
+                                "Northern Europe",
+                                "Southern Europe",
+                                "Western Europe",
+                                "LATIN AMERICA AND THE CARIBBEAN",
+                                "Caribbean",
+                                "Central America",
+                                "South America",
+                                "NORTHERN AMERICA",
+                                "OCEANIA",
+                                "Australia/New Zealand",
+                                "Melanesia",
+                                "Micronesia",
+                                "Polynesia"))
+
+# Changing some country names to match the WHO TB data and ISO codes #
+cbr_wide$country <- (mapvalues((cbr_wide$country), from = c(
+  "United Kingdom",
+  "Dem. People's Republic of Korea",
+  "Micronesia (Fed. States of)",
+  "State of Palestine",
+  "TFYR Macedonia",
+  "Czechia",
+  "C<f4>te d'Ivoire")
+  
+  , to = c(
+    "United Kingdom of Great Britain and Northern Ireland",
+    "Democratic People's Republic of Korea",
+    "Micronesia (Federated States of)",
+    "West Bank and Gaza Strip",
+    "North Macedonia",
+    "Czech Republic",
+    "Republique du Côte d'Ivoire")
+))
+
+
+# add ISO country codes to births
+load("isodict.Rdata")
+code <- read_csv("all.csv") # has more details on codes
+code <- code %>% dplyr::rename(country=name, iso3=`alpha-3`) %>% select(country, `country-code`, iso3, region, `sub-region`)
+
+# Final dataset for crude birth rates
+cbr_wide <- cbr_wide %>% left_join(ISO, by = "country") 
+
+# read in total Population interpolated by single age, annually from 1950 to 2100. 
+# read in population of females in the reproductive age group
+# Annual female population by five-year age group, region, subregion and country, 1950-2100 (thousands)								
+
+popn_all <- read_csv("https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/CSV_FILES/WPP2019_TotalPopulationBySex.csv")
+pop_f <- read_excel("pop_med.xlsx")
+pop_f$"15-24" <- rowSums(pop_f[c("15-19", "20-24")], na.rm=TRUE)
+pop_f$"25-34" <- rowSums(pop_f[c("25-29", "30-34")], na.rm=TRUE)
+pop_f$"35-44" <- rowSums(pop_f[c("35-39", "40-44")], na.rm=TRUE)
+pop_f$"45-54" <- rowSums(pop_f[c("45-49", "50-54")], na.rm=TRUE)
+
+pop_f <- pop_f %>% dplyr::rename(pop_f=Variant, country=`Region, subregion, country or area *`, year=`Reference date (as of 1 July)`)  %>%
+  select(c("country","pop_f", "year", "15-24", "25-34", "35-44", "45-54")) %>% gather(age_group, pop_f, c("15-24", "25-34", "35-44", "45-54"))
+pop_f$pop_f <- pop_f$pop_f*1000
+
+pop_all <- popn_all %>% dplyr::rename(country=Location, year=Time) %>% select(country, year, Variant, PopTotal)%>% 
+  filter(Variant %in% c("Medium", "High", "Low")) 
+                                 
+tpop_wide <- pop_all %>% 
+  group_by_at(vars( -year)) %>%  # group by everything other than the value column. 
+  mutate(row_id=1:n()) %>% ungroup() %>%  # build group index
+  spread(key=Variant, value=PopTotal) %>%    # spread
+  select(-row_id)  # drop the index
+
+
+tpop_wide <- tpop_wide %>% dplyr::rename(tpop_lo=Low, tpop_hi=High, tpop_best=Medium)
+
+pop_f <- pop_f %>% filter(!country %in%
+                            c("WORLD", "More developed regions",
+                              "Less developed regions",
+                              "Least developed countries",
+                              "Less developed regions, excluding least developed countries",
+                              "Less developed regions, excluding China",
+                              "High-income countries",
+                              "Middle-income countries",
+                              "Upper-middle-income countries",
+                              "Lower-middle-income countries",
+                              "Low-income countries",
+                              "Sub-Saharan Africa",
+                              "AFRICA",
+                              "Eastern Africa",
+                              "Middle Africa",
+                              "Northern Africa",
+                              "Western Sahara",
+                              "Southern Africa",
+                              "Western Africa",
+                              "ASIA",
+                              "Eastern Asia",
+                              "South-Central Asia",
+                              "Central Asia",
+                              "Southern Asia",
+                              "South-Eastern Asia",
+                              "Western Asia",
+                              "EUROPE",
+                              "Eastern Europe",
+                              "Northern Europe",
+                              "Southern Europe",
+                              "Western Europe",
+                              "LATIN AMERICA AND THE CARIBBEAN",
+                              "Caribbean",
+                              "Central America",
+                              "South America",
+                              "NORTHERN AMERICA",
+                              "OCEANIA",
+                              "Australia/New Zealand",
+                              "Melanesia",
+                              "Micronesia",
+                              "Polynesia"))
+
+# Changing some country names to match the WHO TB data and ISO codes #
+pop_f$country <- (mapvalues((pop_f$country), from = c(
+  "United Kingdom",
+  "Dem. People's Republic of Korea",
+  "Micronesia (Fed. States of)",
+  "State of Palestine",
+  "TFYR Macedonia",
+  "Czechia")
+  
+  , to = c(
+    "United Kingdom of Great Britain and Northern Ireland",
+    "Democratic People's Republic of Korea",
+    "Micronesia (Federated States of)",
+    "West Bank and Gaza Strip",
+    "North Macedonia",
+    "Czech Republic")
+))
+
+tpop_wide <- tpop_wide %>% filter(!country %in%
+                            c("WORLD", "More developed regions",
+                              "Less developed regions",
+                              "Least developed countries",
+                              "Less developed regions, excluding least developed countries",
+                              "Less developed regions, excluding China",
+                              "High-income countries",
+                              "Middle-income countries",
+                              "Upper-middle-income countries",
+                              "Lower-middle-income countries",
+                              "Low-income countries",
+                              "Sub-Saharan Africa",
+                              "AFRICA",
+                              "Eastern Africa",
+                              "Middle Africa",
+                              "Northern Africa",
+                              "Western Sahara",
+                              "Southern Africa",
+                              "Western Africa",
+                              "ASIA",
+                              "Eastern Asia",
+                              "South-Central Asia",
+                              "Central Asia",
+                              "Southern Asia",
+                              "South-Eastern Asia",
+                              "Western Asia",
+                              "EUROPE",
+                              "Eastern Europe",
+                              "Northern Europe",
+                              "Southern Europe",
+                              "Western Europe",
+                              "LATIN AMERICA AND THE CARIBBEAN",
+                              "Caribbean",
+                              "Central America",
+                              "South America",
+                              "NORTHERN AMERICA",
+                              "OCEANIA",
+                              "Australia/New Zealand",
+                              "Melanesia",
+                              "Micronesia",
+                              "Polynesia"))
+
+# Changing some country names to match the WHO TB data and ISO codes #
+tpop_wide$country <- (mapvalues((tpop_wide$country), from = c(
+  "United Kingdom",
+  "Dem. People's Republic of Korea",
+  "Micronesia (Fed. States of)",
+  "State of Palestine",
+  "TFYR Macedonia",
+  "Czechia")
+  
+  , to = c(
+    "United Kingdom of Great Britain and Northern Ireland",
+    "Democratic People's Republic of Korea",
+    "Micronesia (Federated States of)",
+    "West Bank and Gaza Strip",
+    "North Macedonia",
+    "Czech Republic")
+))
+
+pop_f <- pop_f %>% left_join(ISO, by = "country") %>% select(country,iso3, age_group, year, pop_f)
+tpop_wide <- tpop_wide %>% left_join(ISO, by = "country")
+
+pop_f2017 <- pop_f %>% filter(year==2017) %>% select(-year)
+# pop_all2017 <- pop_all %>% filter(Time==2017) %>% select(-Time) 
+
+num_cbrbirths <- cbr_wide %>% select(-iso2, -iso_numeric) %>% left_join(tpop_wide, by=c("country", "year", "iso3", "g_whoregion")) %>%
+  mutate(cbrbirths_lo = Low_CBR*tpop_lo,
+         cbrbirths_best = Medium_CBR*tpop_best,
+         cbrbirths_hi = High_CBR*tpop_hi)
+
+# num_births$value <- num_births$pop_f * (num_births$value/1000) #  absolute number of births 
+# num_cbrbirths <- num_cbrbirths %>% spread(metric, value) %>% select(-country)
+
+# filter 2017 data to match the WHO TB data 
+# cbr_2017 <- cbr_wide %>% filter(year==2017)
+num_cbrbirths2017 <- num_cbrbirths %>% filter(year==2017)
+
+new_df_pop <- pop_f2017 %>% group_by(country, iso3) %>% summarise(tpopf=sum(pop_f, na.rm = T)) %>% left_join(num_cbrbirths2017, by=c("country","iso3"))
+# combining the different datasets - births, population of females in the reproductive age and TB incident case in women in the reproductive age group
+new_df_cbrbirths <- df_1 %>% group_by(country, iso3) %>%   summarise_at(
+  .vars= vars( best, lo, hi), 
+  .funs =  sum) %>% left_join(new_df_pop, by=c("country", "iso3")) %>%
+  dplyr::rename(TBI_best=best, TBI_lo=lo, TBI_hi=hi)
+new_df_cbrbirths$g_whoregion[is.na(new_df_cbrbirths$g_whoregion)] <- "AFR" # problem with Swaziland
+
+# df_2 <- ISO %>% select(-iso2, -iso3, -iso_numeric) %>% left_join(df, by="country")
+
+# Estimate the incidence of TB in pregnancy
+new_df_cbrbirths$pregTBI_best <- new_df_cbrbirths$cbrbirths_best * 9/12 * (new_df_cbrbirths$TBI_best/new_df_cbrbirths$tpopf)
+new_df_cbrbirths$pregTBI_lo   <- new_df_cbrbirths$cbrbirths_lo   * 9/12 * (new_df_cbrbirths$TBI_lo/new_df_cbrbirths$tpopf)
+new_df_cbrbirths$pregTBI_hi   <- new_df_cbrbirths$cbrbirths_hi   * 9/12 * (new_df_cbrbirths$TBI_hi/new_df_cbrbirths$tpopf)
+
+# Estimate the incidence of TB in post-partum period
+new_df_cbrbirths$ppTBI_best <- new_df_cbrbirths$cbrbirths_best * 3/12 * (new_df_cbrbirths$TBI_best/new_df_cbrbirths$tpopf)
+new_df_cbrbirths$ppTBI_lo   <- new_df_cbrbirths$cbrbirths_lo   * 3/12 * (new_df_cbrbirths$TBI_lo/new_df_cbrbirths$tpopf)
+new_df_cbrbirths$ppTBI_hi   <- new_df_cbrbirths$cbrbirths_hi   * 3/12 * (new_df_cbrbirths$TBI_hi/new_df_cbrbirths$tpopf)
+
+pregTB_births <- new_df_cbrbirths %>% dplyr::group_by(country) %>% summarise(pregTBI_best=sum(pregTBI_best), pregTBI_lo=sum(pregTBI_lo), pregTBI_hi=sum(pregTBI_hi))
+
+ipregTB_births <- new_df_cbrbirths %>% dplyr::group_by(country, iso3) %>% summarise(ipregTBI_best=1.3*(pregTBI_best), ipregTBI_lo=1.3*(pregTBI_lo), ipregTBI_hi=1.3*(pregTBI_hi))
+
+key_parms <- c("tpopf", "TBI_best", "TBI_lo", "TBI_hi", "cbrbirths_best", "cbrbirths_lo", "cbrbirths_hi", "pregTBI_best", 
+               "pregTBI_lo", "pregTBI_hi", "ppTBI_best", 
+               "ppTBI_lo", "ppTBI_hi")
+
+summary_regions2 <- new_df_cbrbirths%>%group_by(g_whoregion)%>%summarise_at(key_parms, funs(sum), na.rm=T)
+
+summary_SEA <- new_df_cbrbirths%>%filter(g_whoregion=="SEA")%>%group_by(country)%>%
+  summarise_at(key_parms, funs(sum), na.rm=T)
+
